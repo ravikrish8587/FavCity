c8374d2c4875b67c244cba646237742830219171
Ravi Krishna <bayankararavikrishna.prathivad@t-mobile.com>
2016-10-17 16:14:18 -0700

    changes

--- FavCity/LANewsVC.swift
+++ FavCity/LANewsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ExploreVC.swift
+// LANewsVC.swift
 //  FavCity
 //
 //  Created by Ravi Krishna on 3/27/16.
@@ -7,29 +7,60 @@
 //
 
 import UIKit
+import WebKit
 
-class ExploreVC: UIViewController {
+class LANewsVC: UIViewController {
 
+   
+    var webView:WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let urlStr:String = "https://developer.apple.com/swift/blog/"
+        let url:URL = URL(string: urlStr)!
+        let request:URLRequest = URLRequest(url: url)
+        webView.load(request)
+        
+        
+    }
+    
+    override func loadView() {
+        webView = WKWebView()               // use load view to 
+        //webView.navigationDelegate = self
+        //view.addSubview(webView)
+        view = webView
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
 
-    /*
-    // MARK: - Navigation
+//    override func viewDidAppear(animated: Bool) {
+//        super.viewDidAppear(animated)
+//        
+//        
+//        
+//        
+//    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+//        webView.userInteractionEnabled = true
+//        webView.scrollView.scrollEnabled = true
+//        webView.contentMode = UIViewContentMode.ScaleAspectFit
+//        print(webView.bounds.width)
+//        print(webView.bounds.height)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let rect = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
+        webView.frame = rect
+        
+
     }
-    */
 
+    @IBAction func goToHome(_ sender: AnyObject) {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
