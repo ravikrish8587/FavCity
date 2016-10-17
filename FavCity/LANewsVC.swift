c7b890d72977506a871f008b073a9e49d1ef48c8
Ravi Krishna <bayankararavikrishna.prathivad@t-mobile.com>
2016-10-17 16:12:25 -0700

    favcity

---
+++ FavCity/LANewsVC.swift
@@ -0,0 +1,35 @@
+//
+//  ExploreVC.swift
+//  FavCity
+//
+//  Created by Ravi Krishna on 3/27/16.
+//  Copyright © 2016 Practice. All rights reserved.
+//
+
+import UIKit
+
+class ExploreVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
